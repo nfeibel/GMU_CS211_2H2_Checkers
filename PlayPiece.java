f86b5eae480836a9d178e043d4957c9bbfd891f1
Ember <74475571+Peppersqueak@users.noreply.github.com>
2020-11-21 18:16:40 -0500

    Update PlayPiece.java

--- PlayPiece.java
+++ PlayPiece.java
@@ -11,6 +11,13 @@ public class PlayPiece extends StackPane {
     private int xLocation;
     private int yLocation;
 
+    /**
+     * Constructor for PlayPiece
+     * 
+     * @param type
+     * @param xLocation
+     * @param yLocation
+     */
     public PlayPiece(PieceType type, int xLocation, int yLocation) {
         this.type = type;
         this.xLocation = xLocation;
@@ -26,6 +33,14 @@ public class PlayPiece extends StackPane {
         pieceActionOnMouseClicked();
     }
 
+    /**
+     * Moves the selected piece from old location to new location
+     *
+     * @param oldXLocation
+     * @param oldYLocation
+     * @param newXLocation
+     * @param newYLocation
+     */
     static void movePiece(int oldXLocation, int oldYLocation, int newXLocation, int newYLocation) {
     	pieceSelected.relocate(SIZEOFTILES * newXLocation, SIZEOFTILES * newYLocation);
     	pieceSelected.setPieceY(newYLocation);
@@ -36,6 +51,14 @@ public class PlayPiece extends StackPane {
         pieceSelected.setOpacity(1.0);
     }
 
+    /**
+     * Lets selected piece attack from old location to new location
+     *
+     * @param oldXLocation
+     * @param oldYLocation
+     * @param newXLocation
+     * @param newYLocation
+     */
     static void attackingPieceMove(int oldXLocation, int oldYLocation, int newXLocation, int newYLocation) {
         int xPieceAttacked = oldXLocation + (newXLocation - oldXLocation) / 2;
         int yPieceAttacked = oldYLocation + (newYLocation - oldYLocation) / 2;
@@ -49,6 +72,12 @@ public class PlayPiece extends StackPane {
         }
     }
 
+    /**
+     * Sets the piece position to (x,y)
+     *
+     * @param x
+     * @param y
+     */
     static void setPiecePosition(int x, int y) {
 
         board[y][x] = new Tile(((x + y) % 2 == 0), x, y);
@@ -68,6 +97,11 @@ public class PlayPiece extends StackPane {
         }
     }
 
+    /**
+     * Creates a piece
+     *
+     * @return ellipse
+     */
     private Ellipse createEllipse(){
         Ellipse ellipse = new Ellipse(SIZEOFTILES * ELLIPSE_RADIUS_X, SIZEOFTILES * ELLIPSE_RADIUS_Y);
         if(type == PieceType.RED){
@@ -84,6 +118,11 @@ public class PlayPiece extends StackPane {
         return ellipse;
     }
 
+    /**
+     * Creates drop shadow for a piece
+     *
+     * @return ellipse
+     */
     private Ellipse createBackround(){
         Ellipse ellipse = new Ellipse(SIZEOFTILES * ELLIPSE_RADIUS_X, SIZEOFTILES * ELLIPSE_RADIUS_Y);
         ellipse.setFill(Color.BLACK);
@@ -95,6 +134,9 @@ public class PlayPiece extends StackPane {
         return ellipse;
     }
 
+    /**
+     * Highlights selected piece
+     */
     private void pieceActionOnMouseClicked() {
         this.setOnMouseClicked(e -> {
             PlayPiece tempPiece = (PlayPiece) e.getSource();
@@ -111,22 +153,47 @@ public class PlayPiece extends StackPane {
         });
     }
 
+    /**
+     * Getter for x location of a PlayPiece
+     *
+     * @return xLocation
+     */
     public int getPieceX() {
         return xLocation;
     }
 
+    /**
+     * Getter for y location of a PlayPiece
+     *
+     * @return yLocation
+     */
     public int getPieceY() {
         return yLocation;
     }
 
+    /**
+     * Setter for x location of a PlayPiece
+     *
+     * @param x
+     */
     public void setPieceX(int x) {
         this.xLocation = x;
     }
 
+    /**
+     * Setter for y location of a PlayPiece
+     *
+     * @param y
+     */
     public void setPieceY(int y) {
         this.yLocation = y;
     }
 
+    /**
+     * Getter for the type of PlayPiece
+     *
+     * @return type
+     */
     public PieceType getType() {
         return type;
     }
