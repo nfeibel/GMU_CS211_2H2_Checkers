1913ed23348812900e5f4ac958ad447fd877ce04
nfeibel <74475274+nfeibel@users.noreply.github.com>
2020-11-21 18:23:52 -0500

    Update PieceType.java

--- PieceType.java
+++ PieceType.java
@@ -1,9 +1,17 @@
-
+package application;
+/**
+ * Represents the Types of Pieces
+ * @author Nick Feibel, Ember Ipek
+ * @version 1.0
+ */
 public enum PieceType {
     RED(1), WHITE(-1);
 
     final int MOVEDIRECTION;
 
+    /*
+     * Constructor for PieceType, and initializes MOVEDIRECTION for Type
+     */
     PieceType(int MOVEDIRECTION){
         this.MOVEDIRECTION = MOVEDIRECTION;
     }
